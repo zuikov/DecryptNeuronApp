682f43397cfa623a5cd04dd4777635189eecd8fc
zuikov <andrei_z@ukr.net>
2022-12-21 11:38:08 +0300

    Add Keras

--- DecryptNeuronApp/DecryptNeuronApp.py
+++ DecryptNeuronApp/DecryptNeuronApp.py
@@ -5,7 +5,10 @@
 # Class to code 8-bit plaintext message
 class CryptoWorker():
     ROUND_KEY = [1, 0, 1, 0, 1, 1, 1, 0];
-    S_BOX_TABLE = [[[0,0,0,0], [0,0,0,1], [0,0,1,0], [0,0,1,1]], [[0,1,0,0], [0,1,0,1], [0,1,1,0], [0,1,1,1]], [[1,0,0,0], [1,0,0,1], [1,0,1,0], [1,0,1,1]], [[1,1,0,0], [1,1,0,1], [1,1,1,0], [1,1,1,1]]];
+    # ordered s-box-table can't be used, becasuse result obviously correlates to theinitial plain text
+    # S_BOX_TABLE = [[[0,0,0,0], [0,0,0,1], [0,0,1,0], [0,0,1,1]], [[0,1,0,0], [0,1,0,1], [0,1,1,0], [0,1,1,1]], [[1,0,0,0], [1,0,0,1], [1,0,1,0], [1,0,1,1]], [[1,1,0,0], [1,1,0,1], [1,1,1,0], [1,1,1,1]]];
+    # use unordered table
+    S_BOX_TABLE = [[[0,1,0,0], [0,1,0,1], [0,1,1,1], [1,1,1,1]], [[1,1,1,0], [0,0,0,1], [0,0,1,0], [0,0,1,1]], [[1,1,0,0], [1,1,0,1], [0,0,0,0], [1,0,0,0]], [[0,1,1,0], [1,0,0,1], [1,0,1,0], [1,0,1,1]]];
     INDEXES_MAP = ['00', '01', '10', '11'];
     HALF_MESSAGE_LENGTH = 4;
     SHIFT_ITERATIONS = 5;
@@ -88,6 +91,42 @@ class CryptoWorker():
         concatenated_message = self.concatenate_message(left_substitution, right_substitution);
         self.mutate_message(concatenated_message);
         return self.shift_message();
+
+
+# Keras Neuron Network
+class KerasDecryptoWorker():
+    from keras import backend as K
+    from keras.models import Model
+    from keras.engine.input_layer import Input
+    from keras.layers.core import Activation, Dense
+    from keras.layers import Flatten, Reshape
+    from keras.layers.convolutional import Conv1D
+    from keras.layers.merging import concatenate
+    from keras.optimizers import Adam, RMSprop
+
+    # Crypto parameters
+    # Use 8-bit messages
+    c_bits = 8
+    pad = 'same'
+
+    nn_input = Input(shape=(c_bits))
+
+    nn_dense1 = Dense(units=(c_bits))(nn_input)
+    nn_dense1a = Activation('tann')(nn_dense1)
+
+    nn_conv1 = Conv1D(filters=2, kernel_size=4, strides=1, padding=pad)(nn_dense1a)
+    nn_conv1a = Activation('tann')(nn_conv1)
+    nn_conv2 = Conv1D(filters=4, kernel_size=2, strides=2, padding=pad)(nn_conv1a)
+    nn_conv2a = Activation('tann')(nn_conv2)
+    nn_conv3 = Conv1D(filters=4, kernel_size=1, strides=1, padding=pad)(nn_conv2a)
+    nn_conv3a = Activation('tann')(nn_conv3)
+    nn_conv4 = Conv1D(filters=1, kernel_size=1, strides=1, padding=pad)(nn_conv3a)
+    nn_conv4a = Activation('tann')(nn_conv4)
+
+    # Attempt at guessing plaintext
+    nn_output = Flatten()(nn_conv4a)
+
+    nn_keras = Model(nn_input, nn_output, name='nn_keras')
 
 
 # Driver Code
